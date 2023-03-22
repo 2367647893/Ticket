ddc912af5a9760f0db40e2fdeea3e403c22f5800
aowuwu <2367647893@qq.com>
2023-03-22 21:19:14 +0800

    feat/登录页布局

--- config/router.config.js
+++ config/router.config.js
@@ -1,12 +1,12 @@
 export default [
   // 配置路由
-  // {
-  //   path: '/user',
-  //   routes: [
-  //     { path: '/user/login', component: './login', title: '登录' },
-  //     { path: '/user/login', component: './login', title: '登录' },
-  //   ],
-  // },
+  {
+    path: '/',
+    routes: [
+      { path: '/login', component: './login', title: '登录' },
+      { path: '/register', component: './register', title: '注册' },
+    ],
+  },
 
   // 配置路由
   {
@@ -16,8 +16,8 @@ export default [
     routes: [
       { path: '/', component: './home', title: '首页' },
       { path: '/user', component: './user', title: '我的' },
-      { path: '/login', component: './login', title: '登录' },
-      { path: '/register', component: './register', title: '注册' },
+      // { path: '/login', component: './login', title: '登录' },
+      // { path: '/register', component: './register', title: '注册' },
       { path: '/services', component: './servicess', title: '客服' },
       { path: '/orderList', component: './orderList', title: '订单' },
     ],
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,14 +1,37 @@
 import React from "react";
-import {connect} from 'dva';
+import { Form, Input, Button } from 'antd-mobile'
+import { connect } from 'dva';
 import './styles.less'
-export default connect((state)=>{
-    return{
+export default connect((state) => {
+    return {
 
     }
 })(login)
-function login(props){
-    return(
-        <div>login</div>
+
+function login(props) {
+    return (
+        <div styleName="LoginBox">
+            <div styleName="HeadBox">
+                <span>ss</span><span>登录</span><span></span>
+            </div>
+            <div styleName="SectionBox">
+                <div styleName="FormBox">
+                    <Form requiredMarkStyle='asterisk'>
+                        <Form.Item name='name' label='用户名' rules={[{ required: true }]}>
+                            <Input placeholder='请输入用户名' />
+                        </Form.Item>
+                        <Form.Item name='name' label='密码' rules={[{ required: true }]}>
+                            <Input placeholder='请输入密码' />
+                        </Form.Item>
+                    </Form>
+                </div>
+                <div styleName="BtnBox">
+                    <Button>登录</Button>
+                    <Button>注册</Button>
+                </div>
+            </div>
+
+        </div>
     )
 }
 //http://10.161.54.76:3000/api/rest/travelList
